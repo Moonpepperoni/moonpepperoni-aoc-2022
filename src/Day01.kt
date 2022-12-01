3d6cf1113d3ce67670a0f17753a4d16a73c18586
Moonpepperoni <jan_leonhardt@icloud.com>
2022-12-01 18:28:09 +0100

    Add Solution for Day1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,27 @@
 fun main() {
+    fun String.toCarriedCalories() = this.lines().sumOf { it.toInt() }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.maxOf(String::toCarriedCalories)
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input
+            .map(String::toCarriedCalories)
+            .sortedDescending()
+            .take(3)
+            .sum()
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = readInputBlankLines("Day01_test")
+    check(part1(testInput) == 24000)
+    check(part2(testInput) == 45000)
 
-    val input = readInput("Day01")
+    val input = readInputBlankLines("Day01")
     println(part1(input))
     println(part2(input))
 }
+
+
+
--- src/Utils.kt
+++ src/Utils.kt
@@ -2,11 +2,16 @@ import java.io.File
 import java.math.BigInteger
 import java.security.MessageDigest
 
+val newLine = System.lineSeparator()
+
 /**
  * Reads lines from the given input txt file.
  */
-fun readInput(name: String) = File("src", "$name.txt")
+fun readInput(name: String) = File("resources", "$name.txt")
     .readLines()
+
+fun readInputBlankLines(name : String) = File("resources", "$name.txt")
+    .readText().split("$newLine$newLine")
 
 /**
  * Converts string to md5 hash.
