725d7aa0dde1244af9586dd7cf692aabd43a66dd
Moonpepperoni <jan_leonhardt@icloud.com>
2022-12-05 23:29:05 +0100

    Add solution for Day03

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,21 +1,58 @@
 fun main() {
+
+    fun String.toRuckSack() : RuckSack {
+        val leftPocket = this.substring(0 until this.length/2).toSet()
+        val rightPocket = this.substring(this.length/2..this.lastIndex).toSet()
+        return RuckSack(leftPocket, rightPocket)
+    }
+
+    fun RuckSack.getCommonChar() : Char {
+        return (this.first intersect this.second).single()
+    }
+
+    fun RuckSackGroup.getCommonChar() : Char {
+        return (this[0] intersect this [1] intersect this[2]).single()
+    }
+
+    fun Char.getScore() : Int {
+        return if (this.isUpperCase()) {
+            this - 'A' + 27
+        } else {
+            this - 'a' + 1
+        }
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.map(String::toRuckSack).map(RuckSack::getCommonChar).sumOf(Char::getScore)
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val groups = input.chunked(3)
+        val rucksackGroups = ArrayList<RuckSackGroup>()
+        for (group in groups) {
+            val rucksackGroup = RuckSackGroup()
+            for (rucksack in group) {
+                rucksackGroup.add(rucksack.toSet())
+            }
+            rucksackGroups.add(rucksackGroup)
+        }
+        return rucksackGroups.map(RuckSackGroup::getCommonChar).sumOf(Char::getScore)
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day03_test")
-    check(part1(testInput) == 24000)
-//    check(part2(testInput) == 45000)
+    check(part1(testInput) == 157)
+    check(part2(testInput) == 70)
 
     val input = readInput("Day03")
     println(part1(input))
-//    println(part2(input))
+    println(part2(input))
 }
+
+typealias RuckSackGroup = ArrayList<Pocket>
+typealias RuckSack = Pair<Pocket, Pocket>
+typealias Pocket = Set<Char>
+
 
 
 
